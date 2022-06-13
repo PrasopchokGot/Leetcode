bc69b975c4d5045a8382a6185a0391207729b0fe
PrasopchokGot <prasopchok.rodsiri@gmail.com>
2022-06-13 21:57:32 +0700

    Add base case and recursive function

--- binary_tree.js
+++ binary_tree.js
@@ -7,15 +7,32 @@ class Node{
 }
 
 const depthFirstValues = root => {
+    if (root == null) return []
+
+    const result = []
     const stack = [root]
     while (stack.length > 0) {
         const current = stack.pop();
-        console.log(current.val);
+        result.push(current.val);
 
+        // Order: Left to Right(stack left is on top of right)
         if (current.right) {stack.push(current.right)}
         if (current.left) {stack.push(current.left)}
     }
+    return result
 };
+
+const depthFirstValues_recursive = root => {
+    if (root == null) return []
+
+    const leftValue = depthFirstValues_recursive(root.left);
+    const rightValue = depthFirstValues_recursive(root.right);
+    
+    // Order: Right to Left
+    return [root.val, ...rightValue, ...leftValue]
+
+
+}
 
 const a = new Node('a');
 const b = new Node('b');
@@ -24,10 +41,17 @@ const d = new Node('d');
 const e = new Node('e');
 const f = new Node('f');
 
+/*
+        a
+        /\
+       b  c        Left to Right: [a, b, d, e, c, f]
+      /\   \       Right to Left: [a, c, f, b, e, d]
+     d  e   f
+*/
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
 c.right = f;
 
-depthFirstValues(a)
+console.log(depthFirstValues_recursive(a))
