575a23bd65b0b9dae96991bf03ac67b8dc5b8699
PrasopchokGot <prasopchok.rodsiri@gmail.com>
2022-06-15 14:54:54 +0700

    max summation of path

---
+++ Algorithm and Data Structure/js/max-root-to-lead.js
@@ -0,0 +1,21 @@
+import {Node} from './Node.js'
+
+const maxPath = (root) => {
+    if (root == null) return 0;
+    const maxValue = Math.max(maxPath(root.left), maxPath(root.right))
+    return maxValue + root.val
+}
+
+const a = new Node(5);
+const b = new Node(11);
+const c = new Node(3);
+const d = new Node(4);
+const e = new Node(2);
+const f = new Node(1);
+// sum of tree = 23
+
+[a.left, a.right] = [b, c];
+[b.left, b.right] = [d, e];
+c.right = f;
+
+console.log(maxPath(a))
